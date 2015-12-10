372f4db674a34976b81634c45a6592bacf49fe5e
seanokeeffe <seanokeeffe28@gmail.com>
2015-12-10 14:54:01 -0800

    Made path relative

--- final_project.R
+++ final_project.R
@@ -13,4 +13,4 @@
 # Get data on trades that happened between 2010 and 2015.
 # 'trades' consists of all the data, which has the team the player was traded from, 
 # the team the player was traded to, the players name, and the date of the trade
-source('/Users/seanokeeffe/Documents/GitHub/MATH154_FinalProject/get_trade_data.R', local=TRUE, echo=FALSE)
+source('get_trade_data.R', local=TRUE, echo=FALSE)
